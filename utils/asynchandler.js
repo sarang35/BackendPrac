059544b7d12b75f29410c480be5255bd370524dd
sarang35 <sarangkrishnams@gmail.com>
2026-05-06 17:02:21 +0530

    Delete utils directory

--- utils/asynchandler.js
+++
@@ -1,6 +0,0 @@
-const asynchandler = (requestHandler) => {
-  return (req, res, nxt) => {
-    Promise.resolve(requestHandler(req, res, nxt)).catch((err) => nxt(err));
-  };
-};
-export default asynchandler;
